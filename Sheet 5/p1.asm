08e9c71976619a724209b5e5c856fc236306d597
Abdelrahman Naser <abdonaser_97@outlook.com>
2017-11-19 23:54:00 +0200

    Update p1.asm
    Write a complete 80x86 assembly language program to prompt for values of x, y
    and z and display the value of the expression: – (x + y – 2 * z + 1).

--- Sheet 5/p1.asm
+++ Sheet 5/p1.asm
@@ -4,12 +4,12 @@
 .STACK 4096
 .DATA
     number1 DWORD ?
-	number2 DWORD ?
-	number3 DWORD ?
+    number2 DWORD ?
+    number3 DWORD ?
 
-	prompt1 BYTE "Enter Number1",0
-	prompt2 BYTE "Enter Number2",0
-	prompt3 BYTE "Enter Number3",0
+	prompt1 BYTE "Enter Number 1",0
+	prompt2 BYTE "Enter Number 2",0
+	prompt3 BYTE "Enter Number 3",0
 
 	string BYTE 20 DUP(?)
 	result BYTE 20 DUP(?)
